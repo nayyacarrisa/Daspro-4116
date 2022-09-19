bda3538d4a1824106d21091ed16e112bca457b4c
nayyacarrisa <113011818+nayyacarrisa@users.noreply.github.com>
2022-09-19 07:35:15 +0700

    Add files via upload

---
+++ DASPRO PERTEMUAN 3/main.cpp
@@ -0,0 +1,34 @@
+/*
+Nama: Nayya
+Nim: A11.2022.14625
+*/
+
+#include <iostream>
+#include <Stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    //printf ("Teks\n");
+
+    int u; //deklarasi
+    u = 18; // assign
+    float t = 162.5; //inisialisasi
+    char ub = 'L';
+    string ubs = "XL";
+
+    cout << "Umur saya: " << u;
+    cout << endl;
+    cout << "Tinggi saya: " << t;
+    cout << endl;
+    cout << "Ukuran baju: " << ubs;
+
+    int a;
+    cout << "\nMasukan angka: ";
+    cin >> a;
+    cout << "Angka anda: " << a;
+
+    return 0;
+}
